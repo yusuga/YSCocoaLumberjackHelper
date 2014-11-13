c08d48445abf2b0ba01257c816742bea5c8204d1
Yu Sugawara <yusuga@me.com>
2014-11-13 09:07:36 +0900

    Update Example

--- Example/YSCocoaLumberjackHelperExample/YSCocoaLumberjackHelperExample/ViewController.m
+++ Example/YSCocoaLumberjackHelperExample/YSCocoaLumberjackHelperExample/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UISegmentedControl *logLevelControl;
+
 @end
 
 @implementation ViewController
@@ -18,17 +20,68 @@
 {
     [super viewDidLoad];
     
+    NSUInteger segmentIdx;
+    switch (ddLogLevel) {
+        case DDLogLevelVerbose:
+            segmentIdx = 5;
+            break;
+        case DDLogLevelDebug:
+            segmentIdx = 4;
+            break;
+        case DDLogLevelInfo:
+            segmentIdx = 3;
+            break;
+        case DDLogLevelWarning:
+            segmentIdx = 2;
+            break;
+        case DDLogLevelError:
+            segmentIdx = 1;
+            break;
+        case DDLogLevelOff:
+        default:
+            segmentIdx = 0;
+            break;
+    }
+    self.logLevelControl.selectedSegmentIndex = segmentIdx;
+    
+    [self log];
+}
+
+- (IBAction)logLevelControlDidChange:(UISegmentedControl *)sender
+{
+    DDLogLevel logLevel;
+    switch ((int)sender.selectedSegmentIndex) {
+        case 5:
+            logLevel = DDLogLevelVerbose;
+            break;
+        case 4:
+            logLevel = DDLogLevelDebug;
+            break;
+        case 3:
+            logLevel = DDLogLevelInfo;
+            break;
+        case 2:
+            logLevel = DDLogLevelWarning;
+            break;
+        case 1:
+            logLevel = DDLogLevelError;
+            break;
+        case 0:
+        default:
+            logLevel = DDLogLevelOff;
+            break;
+    }
+    
+    ddLogLevel = logLevel;
+}
+
+- (IBAction)log
+{
     DDLogError(@"Error: Red color");
     DDLogWarn(@"Warning: Yellow color");
     DDLogInfo(@"Info: Dark gray color");
     DDLogDebug(@"Success: Green color");
     DDLogVerbose(@"Log: Default color");
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
